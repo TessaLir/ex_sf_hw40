9cfad9432189fd4aaf64a906351e59f3422e1725
Valentin Vetyukov <finik001@gmail.com>
2023-03-20 22:07:49 +0300

    Клиент. Базово созданное и очищенное от лишнего React приложение.

---
+++ client/src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styleClass from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styleClass["App"]}>
+      <h3>Стартовое приложение ReactJS</h3>
+    </div>
+  );
+}
+
+export default App;
