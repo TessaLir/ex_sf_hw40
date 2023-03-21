761768006aa2b46a6fed1c978660291679d3bb1c
Valentin Vetyukov <finik001@gmail.com>
2023-03-21 22:37:24 +0300

    Откорректирован App

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+import AppFooter from "./app-footer/app-footer";
+import AppHeader from "./app-header/app-header";
+
 import styleClass from "./App.module.css";
 
 function App() {
   return (
-    <div className={styleClass["App"]}>
-      <h3>Стартовое приложение ReactJS</h3>
-    </div>
+    <>
+      <AppHeader />
+      <h3 className={styleClass["App"]} >Content</h3>
+      <AppFooter />
+    </>
   );
 }
 
